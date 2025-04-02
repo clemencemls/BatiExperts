a9399ea19ac3b53678977ac46c384f5f1d945ff2
Clémence <clemencemalis@hotmail.fr>
2025-04-02 15:06:25 +0200

    ajout de la class order

---
+++ models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+class Order {
+    private int $id;
+    private string $status;
+    private string $title;
+
+    public function getId(){
+        $this->id;
+    }
+
+    public function getStatus(){
+        $this->status;
+    }
+
+    public function getTitle(){
+        $this->title;
+    }
+
+    public function setId(int $id){
+        $this->id=$id;
+    }
+
+    public function setStatus(string $status){
+        $this->status=htmlspecialchars($status);
+    }
+
+    public function setTitle(string $title){
+        $this->title=htmlspecialchars($title);
+    }
+
+}
