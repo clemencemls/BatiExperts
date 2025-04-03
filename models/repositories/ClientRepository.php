5ccc52c0e140f3a91004b48ba9ea01b00665cac4
kays <krys.b@outlook.com>
2025-04-03 12:09:55 +0200

    Repository client réalisé

---
+++ models/repositories/ClientRepository.php
@@ -0,0 +1,93 @@
+<?php 
+
+require_once __DIR__ . "/../Client.php";
+
+class ClientReporitory{
+
+    public DatabaseConnection $connection;
+
+    public function __construct(){
+        $this->connection = new DatabaseConnection();
+    }
+
+    public function addClient(Client $client): bool{
+
+        $statement = $this->connection
+            ->getConnected()
+            ->prepare("INSERT INTO clients (nom, email, telephone) VALUES (:nom, :email, :telephone); ");
+        
+        return $statement->execute([
+            'nom'=>$client->getNom(),
+            'email'=>$client->getEmail(),
+            'telephone'=>$client->getTelephone(),
+        ]);
+
+    }
+
+    public function viewClients(): array{
+
+        $statement = $this->connection
+            ->getConnected()
+            ->prepare("SELECT * FROM clients;");
+        $statement->execute();
+        $statement->fetchAll();
+
+            $clients = [];
+            foreach($statement as $row) {
+                $client = new Client();
+                $client->setId($row['id']);
+                $client->setNom($row['nom']);
+                $client->setEmail($row['email']);
+                $client->setTelephone($row['telephone']);
+
+                $clients[] = $client;
+            }
+
+            return $clients;
+    }
+
+    public function viewClient(int $id): ?Client{
+
+        $statement = $this->connection
+            ->getConnected()
+            ->prepare("SELECT * FROM clients WHERE id = $id");
+        $statement->execute();
+        $result=$statement->fetch();
+
+        if (!$result){
+            return null;
+        }
+
+        $client = new Client();
+        $client->setId($result['id']);
+        $client->setNom($result['nom']);
+        $client->setEmail($result['email']);
+        $client->setTelephone($result['telephone']);
+
+        return $client;
+    }
+
+    public function updateClient(Client $client): bool{
+
+        $statement = $this->connection
+            ->getConnected()
+            ->prepare("UPDATE clients SET nom = :nom, email = :email, telephone = :telephone WHERE id = :id");
+
+        return $statement->execute([
+            'nom'=>$client->getNom(),
+            'email'=>$client->getEmail(),
+            'telephone'=>$client->getTelephone(),
+        ]);
+    }
+
+    public function deleteClient(int $id): bool{
+
+        $statement = $this->connection
+            ->getConnected()
+            ->prepare("DELETE FROM clients WHERE id=$id");
+        
+        return $statement -> execute([
+            'id' => $id
+        ]);
+    }
+}
